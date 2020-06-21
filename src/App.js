c08d859cb19a03609b16cbcf312e5dde41c6c25f
KeithW <>
2020-06-21 11:22:41 +0100

    Added sass to the project and banner to show env

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Banner from "./Components/Banner";
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         HELLO WORLD FROM KW
